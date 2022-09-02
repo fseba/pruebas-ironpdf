09301cb9d62e689b3b50c50033441783d457ac98
Sebastian Flores <s.flores@outlook.com.ar>
2022-09-01 21:20:04 -0300

    Agrego archivos

---
+++ Converters.cs
@@ -0,0 +1,58 @@
+﻿using IronPdf;
+
+namespace PruebaIronPDF
+{
+    internal class PDFConverter
+    {
+        private string outputPath;
+
+        public PDFConverter(string outputPath)
+        {
+            this.outputPath = outputPath;
+        }
+
+        public void StringHTMLToPDF(string htmlStr, string pdfFileName)
+        {
+            // Create a PDF from an existing HTML using C#
+            var Renderer = new HtmlToPdf();
+            var PDF = Renderer.RenderHtmlAsPdf(htmlStr);
+
+            Console.WriteLine($@" - Output: {outputPath}\{pdfFileName}.pdf");            
+            PDF.SaveAs($@"{outputPath}\{pdfFileName}.pdf");
+            Console.WriteLine(" - Finalizó correctamente");
+        }
+
+        public void HTMLFileToPDF(string htmlFilePath, string pdfFileName)
+        {
+            // Install IronPdf with Nuget:  PM> Install-Package IronPdf
+            HtmlToPdf Renderer = new HtmlToPdf();
+            
+            //Choose screen or print CSS media
+            Renderer.PrintOptions.CssMediaType = PdfPrintOptions.PdfCssMediaType.Screen;
+            
+            //Set the width of the responsive virtual browser window in pixels
+            Renderer.PrintOptions.ViewPortWidth = 1280;
+            
+            // Render an HTML document or snippet as a string
+            var PDF = Renderer.RenderHTMLFileAsPdf(htmlFilePath);
+
+            Console.WriteLine($@" - Output: {outputPath}\{pdfFileName}.pdf");            
+            PDF.SaveAs($@"{outputPath}\{pdfFileName}.pdf");
+            Console.WriteLine(" - Finalizó correctamente");
+        }
+
+        public void HTMLWithJavaScriptToPDF(string htmlFilePath, string pdfFileName)
+        {
+            var Renderer = new HtmlToPdf();
+            Renderer.PrintOptions.EnableJavaScript = true;
+            Renderer.PrintOptions.RenderDelay = 500;
+            Renderer.PrintOptions.CssMediaType = PdfPrintOptions.PdfCssMediaType.Print;
+
+            var PDF = Renderer.RenderHTMLFileAsPdf(htmlFilePath);
+            
+            Console.WriteLine($@" - Output: {outputPath}\{pdfFileName}.pdf");            
+            PDF.SaveAs($@"{outputPath}\{pdfFileName}.pdf");
+            Console.WriteLine(" - Finalizó correctamente");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using PruebaIronPDF;
+string basePath = "";
+#if DEBUG
+basePath = @"C:\Users\sflor\Desarrollo\IronPDF\PruebaIronPDF";
+#else
+basePath = @$"{Directory.GetCurrentDirectory()}";
+#endif
+
+string inputPath = $@"{basePath}\Input";
+string outputPath = $@"{basePath}\Output";
+
+if (!Directory.Exists(outputPath))
+    Directory.CreateDirectory(outputPath);
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Paso 1: String HTML a PDF");
+Console.WriteLine(" - Espere un momento...");
+
+PDFConverter converter = new PDFConverter(outputPath);
+var htmlStr = "<h1>Ironsoftware</h1>";
+converter.StringHTMLToPDF(htmlStr, "1-String_HTML");
+
+Console.WriteLine("Paso 2: Archivo HTML a PDF");
+Console.WriteLine(" - Espere un momento...");
+var htmlFilePath = $@"{inputPath}\InvoiceTemplate.html";
+converter.HTMLFileToPDF(htmlFilePath, "2-HTML_File");
+
+Console.WriteLine("Paso 3: Archivo HTML con JavaScript a PDF");
+Console.WriteLine(" - Espere un momento...");
+var htmlFilePath2 = $@"{inputPath}\D3jsSample.html";
+converter.HTMLFileToPDF(htmlFilePath2, "3-HTML_JS_File");
+
+Console.WriteLine(" ");
+Console.WriteLine("Trabajo terminado, presione una tecla para finalizar...");
+Console.ReadLine();
